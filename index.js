bbfe77a42b28f9983ca116bbe575d420290d9c9c
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-05-14 03:00:39 +0600

    added cancel part

--- index.js
+++ index.js
@@ -121,6 +121,13 @@ async function run() {
       res.send(result);
      })
 
+     app.delete('/requests/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await requestsCollection.deleteOne(query);
+      res.send(result);
+     })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
