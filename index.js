62c0df751bf79322bef8d3a74b2d34e261bf99cd
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-05-14 02:19:04 +0600

    fetching request post by email

--- index.js
+++ index.js
@@ -113,6 +113,14 @@ async function run() {
       res.send(result)
      })
 
+
+     app.get('/requests/:email', async(req,res)=>{
+      const email = req.params.email;
+      const query = {volunteerEmail: email};
+      const result = await requestsCollection.find(query).toArray();
+      res.send(result);
+     })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
